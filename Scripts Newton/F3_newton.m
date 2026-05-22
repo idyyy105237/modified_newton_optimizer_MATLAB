9de412d97df2e15300e7f8bb27ed50b180a86151
Catarina <a108558@uminho.pt>
2026-05-22 17:19:30 +0100

    scripts newton

---
+++ Scripts Newton/F3_newton.m
@@ -0,0 +1,16 @@
+clear, clc, close all;
+
+%% Paths para os problemas e para a função MN_modificado1.m
+path(path,'../Problemas/GradHess') % f com o grad e hess
+path(path,'../Problemas/F')  % f
+path(path, '../');  % MN_modificado 1
+
+%% Ponto inicial: (0,0)
+w0 = [0;0];
+
+%% Para critério de paragem
+epsilon = 1e-6;  
+Kmax = 1000;  
+
+%% Aplicar o algoritmo Método de Newton Modificado
+[w_opt, Fval_opt, output] = MN_modificado1(@F3, @F3_with_grad_hess, w0, epsilon, Kmax)
